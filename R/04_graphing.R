4abe2ad597486faeabb44add46479a49432e4c45
d-girard <danielle.girard@mail.mcgill.ca>
2019-06-14 11:30:17 -0400

    add graphing script and edit to data import

---
+++ R/04_graphing.R
@@ -0,0 +1,87 @@
+############## GRAPHING #################################
+
+source("R/01_helper_functions.R")
+
+# Set up timeframes
+year_prior <- as.POSIXlt(End_date)
+year_prior$year <- year_prior$year - 1
+year_prior_prior <- as.POSIXlt(year_prior)
+year_prior_prior$year <- year_prior$year - 1
+
+## ACTIVE AIRBNBS OVER TIME
+
+figure2 <- ggplot(daily %>% 
+                    group_by(Date) %>% 
+                    summarize(Listings = n())) +
+  geom_line(aes(Date, Listings)) +
+  theme_minimal()
+
+#ggsave("output/figure2.jpg")
+
+
+## HOST REVENUE
+
+host_revenue<-
+  daily %>%
+  filter(Date >= year_prior, Date <= End_date, Status == "R") %>%
+  group_by(Airbnb_HID) %>%
+  summarize(rev = sum(Price)) %>%
+  filter(rev > 0) %>%
+  summarize(
+    `Top 1%`  = sum(rev[rev > quantile(rev, c(0.99))] / sum(rev)),
+    `Top 5%`  = sum(rev[rev > quantile(rev, c(0.95))] / sum(rev)),
+    `Top 10%` = sum(rev[rev > quantile(rev, c(0.90))] / sum(rev))) %>% 
+  gather(`Top 1%`, `Top 5%`, `Top 10%`, key = "percentile", value = "value") %>% 
+  mutate(percentile = factor(percentile, levels = c('Top 1%', 'Top 5%', 'Top 10%')))
+
+figure3 <- 
+  ggplot(host_revenue)+
+  geom_bar(mapping = aes(x = percentile, y = value, fill = percentile), stat = "identity")+
+  theme_minimal()+
+  scale_fill_manual("Percentile", values = alpha(c("lightblue", "blue", "darkblue"), .6))+
+  theme(axis.title.y = element_blank()) +
+  theme(axis.title.x = element_blank())
+
+#ggsave("output/figure3.jpg")
+
+# Revenue over past twelve months and twelve months prior to that
+
+daily %>% 
+  filter(Date <= End_date & 
+           Date >= year_prior &
+           Status == "R" ) %>%
+  summarise(sum_revenue = sum(Price, na.rm = TRUE) * exchange_rate)
+
+daily %>% 
+  filter(Date <= year_prior 
+         & Date >= year_prior_prior 
+         & Status == "R") %>%
+  summarise(sum_revenue = sum(Price, na.rm = TRUE) * exchange_rate)
+
+
+
+# Housing loss on the end date and a year prior
+
+st_drop_geometry(strr_ghost(property, Property_ID, Airbnb_HID, Created, Scraped, year_prior,
+                            End_date, listing_type = Listing_Type) %>% 
+                   filter(date == End_date) %>% 
+                   group_by(ghost_ID) %>% 
+                   summarize(n = sum(housing_units)) %>% 
+                   ungroup() %>% 
+                   summarize(GH_housing_loss = sum(n))) +
+  nrow(daily %>% 
+         filter(Date == End_date) %>% 
+         inner_join(property, .) %>% 
+         filter(FREH == TRUE)) 
+
+st_drop_geometry(strr_ghost(property, Property_ID, Airbnb_HID, Created, Scraped, year_prior_prior,
+                            year_prior, listing_type = Listing_Type) %>% 
+                   filter(date == year_prior) %>% 
+                   group_by(ghost_ID) %>% 
+                   summarize(n = sum(housing_units)) %>% 
+                   ungroup() %>% 
+                   summarize(GH_housing_loss = sum (n))) +
+  nrow(daily %>% 
+         filter(Date == year_prior) %>% 
+         inner_join(property, .) %>% 
+         filter(FREH == TRUE))
