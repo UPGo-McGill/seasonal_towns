5cf20729f09953909c95fa2bed0ee7a40e5b8c7f
d-girard <danielle.girard@mail.mcgill.ca>
2019-06-19 12:37:15 -0400

    calculate numbers

--- R/05_numbers.R
+++ R/05_numbers.R
@@ -2,18 +2,55 @@
 #### AND RAFFLE #######
 
 ## Number of Active Listings last year
-round(daily %>% 
+listings2019 <- daily %>% 
         filter(Date <= End_date & Date >= year_prior) %>% 
         group_by(Date) %>% 
         summarize(Listings = n()) %>%
-        summarise(mean_Listings = mean(Listings)))
+        summarise(mean_Listings = mean(Listings))%>%
+        as.numeric()
 
 ## Number of Active Listings year prior
-round(daily %>% 
+daily %>% 
         filter(Date <= year_prior & Date >= year_prior_prior) %>% 
         group_by(Date) %>% 
         summarize(Listings = n()) %>%
-        summarise(mean_Listings = mean(Listings)))
+        summarise(mean_Listings = mean(Listings))
+
+
+## Revenue last year
+rev2019 <- daily %>% 
+  filter(Date <= Date & 
+           Date >= year_prior &
+           Status == "R" ) %>%
+  summarise(sum_revenue = sum(Price, na.rm = TRUE) * exchange_rate)%>%
+  as.numeric()
+
+## Revenue year prior
+daily %>% 
+  filter(Date <= year_prior & 
+           Date >= year_prior_prior &
+           Status == "R" ) %>%
+  summarise(sum_revenue = sum(Price, na.rm = TRUE) * exchange_rate)
+
+## Revenue per host
+
+host_revenue<-
+  daily %>%
+  filter(Date >= year_prior, Date <= End_date, Status == "R") %>%
+  group_by(Airbnb_HID) %>%
+  summarize(rev = sum(Price)) %>%
+  filter(rev > 0) %>%
+  summarize(
+    `Top 1%`  = sum(rev[rev > quantile(rev, c(0.99))] / sum(rev)),
+    `Top 5%`  = sum(rev[rev > quantile(rev, c(0.95))] / sum(rev)),
+    `Top 10%` = sum(rev[rev > quantile(rev, c(0.90))] / sum(rev))) %>% 
+  gather(`Top 1%`, `Top 5%`, `Top 10%`, key = "percentile", value = "value") %>% 
+  mutate(percentile = factor(percentile, levels = c('Top 1%', 'Top 5%', 'Top 10%')))
+
+host_revenue%>%
+  mutate(revenue = value * rev2019)
+
+##
 
 ## List by Host ID
 property%>%
