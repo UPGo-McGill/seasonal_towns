3bfbfff912b692efec0899bc9d1da40b74fd0f41
d-girard <danielle.girard@mail.mcgill.ca>
2019-07-04 10:07:07 -0400

    draft scripts

---
+++ R/non_PEC.R
@@ -0,0 +1,293 @@
+
+Province_Codes <- filter(list_census_regions(dataset = "CA16"), level == "PR") 
+pr <- "35"
+canada <- 
+  get_census(
+    dataset = "CA16", 
+    regions = list(PR = "35", PR = "24", PR="59", PR="48", PR="46", PR = "47", PR ="12", PR="13", PR="10", PR="11", PR="61", PR="62", PR="60"),  
+    level = "CD",
+    geo_format = "sf") %>% 
+  st_transform(32618)
+
+nunavut <- 
+  get_census(
+    dataset = "CA16", 
+    regions = list(PR = "62"),  
+    level = "CD",
+    geo_format = "sf") %>% 
+  st_transform(32618)
+
+plot(nunavut$geometry)
+plot(canada$geometry)
+
+
+property_blue_mountains <-
+  read_csv("data/blue_mountains_property.csv", col_types = cols_only(
+    `Property_ID` = col_character(),
+    `Listing_Title` = col_character(),
+    `Property_Type` = col_character(),
+    `Listing_Type` = col_character(),
+    `Created_F` = col_skip(),
+    `Created` = col_date(format = ""),
+    `Scraped_F` = col_skip(),
+    `Scraped` = col_date(format = ""),
+    `Latitude` = col_double(),
+    `Longitude` = col_double(),
+    `Airbnb_PID` = col_double(),
+    `Airbnb_HID` = col_double()))%>%
+  set_names(c("Property_ID", "Listing_Title", "Property_Type", "Listing_Type",
+              "Created", "Scraped", "Latitude", "Longitude", "Airbnb_PID",
+              "Airbnb_HID")) %>% 
+  arrange(Property_ID) %>% 
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+  st_transform(32618) %>% 
+  filter(Property_Type %in% c(
+    "House", "Private room in house", "Apartment", "Cabin",
+    "Entire condominium", "Townhouse", "Condominium", "Entire apartment",
+    "Private room", "Loft", "Place", "Entire house", "Villa", "Guesthouse",
+    "Private room in apartment", "Guest suite", "Shared room in dorm",
+    "Chalet", "Dorm", "Entire chalet", "Shared room in loft", "Cottage",
+    "Resort", "Serviced apartment", "Other", "Bungalow", "Farm stay",
+    "Private room in villa", "Entire loft", "Entire villa",
+    "Private room in guesthouse", "Island", "Entire cabin", "Vacation home",
+    "Entire bungalow", "Earth house", "Nature lodge", "In-law",
+    "Entire guest suite", "Shared room in apartment", "Private room in loft",
+    "Tiny house", "Castle", "Earth House", "Private room in condominium",
+    "Entire place", "Shared room", "Hut", "Private room in guest suite",
+    "Private room in townhouse", "Timeshare", "Entire townhouse",
+    "Shared room in house", "Entire guesthouse", "Shared room in condominium",
+    "Cave", "Private room in cabin", "Dome house",
+    "Private room in vacation home", "Private room in dorm",
+    "Entire serviced apartment", "Private room in bungalow",
+    "Private room in serviced apartment", "Entire Floor", "Entire earth house",
+    "Entire castle", "Shared room in chalet", "Shared room in bungalow",
+    "Shared room in townhouse", "Entire cottage", "Private room in castle",
+    "Private room in chalet", "Private room in nature lodge", "Entire in-law",
+    "Shared room in guesthouse", "Casa particular", "Serviced flat", "Minsu",
+    "Entire timeshare", "Shared room in timeshare", "Entire vacation home",
+    "Entire nature lodge", "Entire island", "Private room in in-law",
+    "Shared room in serviced apartment", "Shared room in cabin", "Entire dorm",
+    "Entire cave", "Private room in timeshare", "Shared room in guest suite",
+    "Private room in cave", "Entire tiny house",
+    "Private room in casa particular (cuba)", "Casa particular (cuba)",
+    "Private room in cottage", "Private room in tiny house",
+    "Entire casa particular", "")) %>% 
+  select(-Property_Type)
+
+
+
+####
+property_mont_tremblant <-
+  read_csv("data/mont_tremblant_property.csv", col_types = cols_only(
+    `Property_ID` = col_character(),
+    `Listing_Title` = col_character(),
+    `Property_Type` = col_character(),
+    `Listing_Type` = col_character(),
+    `Created_F` = col_skip(),
+    `Created` = col_date(format = ""),
+    `Scraped_F` = col_skip(),
+    `Scraped` = col_date(format = ""),
+    `Latitude` = col_double(),
+    `Longitude` = col_double(),
+    `Airbnb_PID` = col_double(),
+    `Airbnb_HID` = col_double()))%>%
+  set_names(c("Property_ID", "Listing_Title", "Property_Type", "Listing_Type",
+              "Created", "Scraped", "Latitude", "Longitude", "Airbnb_PID",
+              "Airbnb_HID")) %>% 
+  arrange(Property_ID) %>% 
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+  st_transform(32618) %>% 
+  filter(Property_Type %in% c(
+    "House", "Private room in house", "Apartment", "Cabin",
+    "Entire condominium", "Townhouse", "Condominium", "Entire apartment",
+    "Private room", "Loft", "Place", "Entire house", "Villa", "Guesthouse",
+    "Private room in apartment", "Guest suite", "Shared room in dorm",
+    "Chalet", "Dorm", "Entire chalet", "Shared room in loft", "Cottage",
+    "Resort", "Serviced apartment", "Other", "Bungalow", "Farm stay",
+    "Private room in villa", "Entire loft", "Entire villa",
+    "Private room in guesthouse", "Island", "Entire cabin", "Vacation home",
+    "Entire bungalow", "Earth house", "Nature lodge", "In-law",
+    "Entire guest suite", "Shared room in apartment", "Private room in loft",
+    "Tiny house", "Castle", "Earth House", "Private room in condominium",
+    "Entire place", "Shared room", "Hut", "Private room in guest suite",
+    "Private room in townhouse", "Timeshare", "Entire townhouse",
+    "Shared room in house", "Entire guesthouse", "Shared room in condominium",
+    "Cave", "Private room in cabin", "Dome house",
+    "Private room in vacation home", "Private room in dorm",
+    "Entire serviced apartment", "Private room in bungalow",
+    "Private room in serviced apartment", "Entire Floor", "Entire earth house",
+    "Entire castle", "Shared room in chalet", "Shared room in bungalow",
+    "Shared room in townhouse", "Entire cottage", "Private room in castle",
+    "Private room in chalet", "Private room in nature lodge", "Entire in-law",
+    "Shared room in guesthouse", "Casa particular", "Serviced flat", "Minsu",
+    "Entire timeshare", "Shared room in timeshare", "Entire vacation home",
+    "Entire nature lodge", "Entire island", "Private room in in-law",
+    "Shared room in serviced apartment", "Shared room in cabin", "Entire dorm",
+    "Entire cave", "Private room in timeshare", "Shared room in guest suite",
+    "Private room in cave", "Entire tiny house",
+    "Private room in casa particular (cuba)", "Casa particular (cuba)",
+    "Private room in cottage", "Private room in tiny house",
+    "Entire casa particular", "")) %>% 
+  select(-Property_Type)
+
+
+####
+property_whistler <-
+  read_csv("data/whistler_property.csv", col_types = cols_only(
+    `Property_ID` = col_character(),
+    `Listing_Title` = col_character(),
+    `Property_Type` = col_character(),
+    `Listing_Type` = col_character(),
+    `Created_F` = col_skip(),
+    `Created` = col_date(format = ""),
+    `Scraped_F` = col_skip(),
+    `Scraped` = col_date(format = ""),
+    `Latitude` = col_double(),
+    `Longitude` = col_double(),
+    `Airbnb_PID` = col_double(),
+    `Airbnb_HID` = col_double()))%>%
+  set_names(c("Property_ID", "Listing_Title", "Property_Type", "Listing_Type",
+              "Created", "Scraped", "Latitude", "Longitude", "Airbnb_PID",
+              "Airbnb_HID")) %>% 
+  arrange(Property_ID) %>% 
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+  st_transform(32618) %>% 
+  filter(Property_Type %in% c(
+    "House", "Private room in house", "Apartment", "Cabin",
+    "Entire condominium", "Townhouse", "Condominium", "Entire apartment",
+    "Private room", "Loft", "Place", "Entire house", "Villa", "Guesthouse",
+    "Private room in apartment", "Guest suite", "Shared room in dorm",
+    "Chalet", "Dorm", "Entire chalet", "Shared room in loft", "Cottage",
+    "Resort", "Serviced apartment", "Other", "Bungalow", "Farm stay",
+    "Private room in villa", "Entire loft", "Entire villa",
+    "Private room in guesthouse", "Island", "Entire cabin", "Vacation home",
+    "Entire bungalow", "Earth house", "Nature lodge", "In-law",
+    "Entire guest suite", "Shared room in apartment", "Private room in loft",
+    "Tiny house", "Castle", "Earth House", "Private room in condominium",
+    "Entire place", "Shared room", "Hut", "Private room in guest suite",
+    "Private room in townhouse", "Timeshare", "Entire townhouse",
+    "Shared room in house", "Entire guesthouse", "Shared room in condominium",
+    "Cave", "Private room in cabin", "Dome house",
+    "Private room in vacation home", "Private room in dorm",
+    "Entire serviced apartment", "Private room in bungalow",
+    "Private room in serviced apartment", "Entire Floor", "Entire earth house",
+    "Entire castle", "Shared room in chalet", "Shared room in bungalow",
+    "Shared room in townhouse", "Entire cottage", "Private room in castle",
+    "Private room in chalet", "Private room in nature lodge", "Entire in-law",
+    "Shared room in guesthouse", "Casa particular", "Serviced flat", "Minsu",
+    "Entire timeshare", "Shared room in timeshare", "Entire vacation home",
+    "Entire nature lodge", "Entire island", "Private room in in-law",
+    "Shared room in serviced apartment", "Shared room in cabin", "Entire dorm",
+    "Entire cave", "Private room in timeshare", "Shared room in guest suite",
+    "Private room in cave", "Entire tiny house",
+    "Private room in casa particular (cuba)", "Casa particular (cuba)",
+    "Private room in cottage", "Private room in tiny house",
+    "Entire casa particular", "")) %>% 
+  select(-Property_Type)
+
+
+
+property_pec <-
+  read_csv("data/PEC_property.csv", col_types = cols_only(
+    `Property_ID` = col_character(),
+    `Listing_Title` = col_character(),
+    `Property_Type` = col_character(),
+    `Listing_Type` = col_character(),
+    `Created` = col_date(format = ""),
+    `Scraped` = col_date(format = ""),
+    Latitude = col_double(),
+    Longitude = col_double(),
+    `City` = col_skip(),
+    `Airbnb_PID` = col_double(),
+    `Airbnb_HID` = col_double(),
+    `HomeAway_PID` = col_skip(),
+    `HomeAway_HID` = col_skip())) %>% 
+  set_names(c("Property_ID", "Listing_Title", "Property_Type", "Listing_Type",
+              "Created", "Scraped", "Latitude", "Longitude", "Airbnb_PID",
+              "Airbnb_HID")) %>% 
+  arrange(Property_ID) %>% 
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+  st_transform(32618) %>% 
+  filter(Property_Type %in% c(
+    "House", "Private room in house", "Apartment", "Cabin",
+    "Entire condominium", "Townhouse", "Condominium", "Entire apartment",
+    "Private room", "Loft", "Place", "Entire house", "Villa", "Guesthouse",
+    "Private room in apartment", "Guest suite", "Shared room in dorm",
+    "Chalet", "Dorm", "Entire chalet", "Shared room in loft", "Cottage",
+    "Resort", "Serviced apartment", "Other", "Bungalow", "Farm stay",
+    "Private room in villa", "Entire loft", "Entire villa",
+    "Private room in guesthouse", "Island", "Entire cabin", "Vacation home",
+    "Entire bungalow", "Earth house", "Nature lodge", "In-law",
+    "Entire guest suite", "Shared room in apartment", "Private room in loft",
+    "Tiny house", "Castle", "Earth House", "Private room in condominium",
+    "Entire place", "Shared room", "Hut", "Private room in guest suite",
+    "Private room in townhouse", "Timeshare", "Entire townhouse",
+    "Shared room in house", "Entire guesthouse", "Shared room in condominium",
+    "Cave", "Private room in cabin", "Dome house",
+    "Private room in vacation home", "Private room in dorm",
+    "Entire serviced apartment", "Private room in bungalow",
+    "Private room in serviced apartment", "Entire Floor", "Entire earth house",
+    "Entire castle", "Shared room in chalet", "Shared room in bungalow",
+    "Shared room in townhouse", "Entire cottage", "Private room in castle",
+    "Private room in chalet", "Private room in nature lodge", "Entire in-law",
+    "Shared room in guesthouse", "Casa particular", "Serviced flat", "Minsu",
+    "Entire timeshare", "Shared room in timeshare", "Entire vacation home",
+    "Entire nature lodge", "Entire island", "Private room in in-law",
+    "Shared room in serviced apartment", "Shared room in cabin", "Entire dorm",
+    "Entire cave", "Private room in timeshare", "Shared room in guest suite",
+    "Private room in cave", "Entire tiny house",
+    "Private room in casa particular (cuba)", "Casa particular (cuba)",
+    "Private room in cottage", "Private room in tiny house",
+    "Entire casa particular", "")) %>% 
+  select(-Property_Type)
+
+
+property_banff <- property_banff%>%
+  mutate(region="Banff")
+property_blue_mountains <- property_blue_mountains%>%
+  mutate(region="Blue Mountains")
+property_mont_tremblant <- property_mont_tremblant%>%
+  mutate(region="Mont Tremblant")
+property_pec <- property_pec%>%
+  mutate(region="Prince Edward County")
+property_whistler <- property_whistler%>%
+  mutate(region="Whistler")
+
+tm_shape(canada)+
+  tm_lines(col="black")+
+  tm_shape(property_banff)+
+  tm_dots(col="blue")+
+  tm_shape(property_blue_mountains)+
+  tm_dots(col="red")+
+  tm_shape(property_mont_tremblant)+
+  tm_dots(col="green")+
+  tm_shape(property_pec)+
+  tm_dots(col="purple")+
+  tm_shape(property_whistler)+
+  tm_dots(col="black")
+  
+prop_whistler <- property_whistler%>%
+  st_drop_geometry()
+prop_banff <- property_banff%>%
+  st_drop_geometry()
+prop_pec <- property_pec%>%
+  st_drop_geometry()
+prop_mont_t <- property_mont_tremblant%>%
+  st_drop_geometry()
+prop_blue_m <- property_blue_mountains%>%
+  st_drop_geometry()
+
+
+props <- rbind(property_banff, property_blue_mountains, property_mont_tremblant, property_pec, property_whistler)
+
+props%>%
+  group_by(region)
+
+regions <- factor(c("Banff","Mont Tremblant","Blue Mountains","Whistler", "PEC"))
+props <- data.frame(regions= regions)
+
+
+active2019 <- numeric(5)
+active2019[1] <- 
+props <- props%>%
+  data.frame()
