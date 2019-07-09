e9ffd8962f360b3bac885f3cafb1995c4594aa8d
d-girard <danielle.girard@mail.mcgill.ca>
2019-07-09 14:52:55 -0400

    tourist town analysis

--- R/non_PEC.R
+++ R/non_PEC.R
@@ -63,6 +63,58 @@ property_blue_mountains <-
     "Entire casa particular", "")) %>% 
   select(-Property_Type)
 
+property_banff <-
+  read_csv("data/banff_property.csv", col_types = cols_only(
+    `Property_ID` = col_character(),
+    `Listing_Title` = col_character(),
+    `Property_Type` = col_character(),
+    `Listing_Type` = col_character(),
+    `Created_F` = col_skip(),
+    `Created` = col_date(format = ""),
+    `Scraped_F` = col_skip(),
+    `Scraped` = col_date(format = ""),
+    `Latitude` = col_double(),
+    `Longitude` = col_double(),
+    `Airbnb_PID` = col_double(),
+    `Airbnb_HID` = col_double()))%>%
+  set_names(c("Property_ID", "Listing_Title", "Property_Type", "Listing_Type",
+              "Created", "Scraped", "Latitude", "Longitude", "Airbnb_PID",
+              "Airbnb_HID")) %>% 
+  arrange(Property_ID) %>% 
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+  st_transform(32618) %>% 
+  filter(Property_Type %in% c(
+    "House", "Private room in house", "Apartment", "Cabin",
+    "Entire condominium", "Townhouse", "Condominium", "Entire apartment",
+    "Private room", "Loft", "Place", "Entire house", "Villa", "Guesthouse",
+    "Private room in apartment", "Guest suite", "Shared room in dorm",
+    "Chalet", "Dorm", "Entire chalet", "Shared room in loft", "Cottage",
+    "Resort", "Serviced apartment", "Other", "Bungalow", "Farm stay",
+    "Private room in villa", "Entire loft", "Entire villa",
+    "Private room in guesthouse", "Island", "Entire cabin", "Vacation home",
+    "Entire bungalow", "Earth house", "Nature lodge", "In-law",
+    "Entire guest suite", "Shared room in apartment", "Private room in loft",
+    "Tiny house", "Castle", "Earth House", "Private room in condominium",
+    "Entire place", "Shared room", "Hut", "Private room in guest suite",
+    "Private room in townhouse", "Timeshare", "Entire townhouse",
+    "Shared room in house", "Entire guesthouse", "Shared room in condominium",
+    "Cave", "Private room in cabin", "Dome house",
+    "Private room in vacation home", "Private room in dorm",
+    "Entire serviced apartment", "Private room in bungalow",
+    "Private room in serviced apartment", "Entire Floor", "Entire earth house",
+    "Entire castle", "Shared room in chalet", "Shared room in bungalow",
+    "Shared room in townhouse", "Entire cottage", "Private room in castle",
+    "Private room in chalet", "Private room in nature lodge", "Entire in-law",
+    "Shared room in guesthouse", "Casa particular", "Serviced flat", "Minsu",
+    "Entire timeshare", "Shared room in timeshare", "Entire vacation home",
+    "Entire nature lodge", "Entire island", "Private room in in-law",
+    "Shared room in serviced apartment", "Shared room in cabin", "Entire dorm",
+    "Entire cave", "Private room in timeshare", "Shared room in guest suite",
+    "Private room in cave", "Entire tiny house",
+    "Private room in casa particular (cuba)", "Casa particular (cuba)",
+    "Private room in cottage", "Private room in tiny house",
+    "Entire casa particular", "")) %>% 
+  select(-Property_Type)
 
 
 ####
@@ -332,12 +384,44 @@ property_whistler <- property_whistler%>%
 #regions <- factor(c("Banff","Mont Tremblant","Blue Mountains","Whistler", "PEC"))
 #props <- data.frame(regions= regions)
 
+## BANFF
+  
+  ## filter out homeaway listings
+  property_banff%>%
+    filter(str_detect(Property_ID, "ab"))
+  
+  ## Analysis: number of listings per host
+  property_banff%>%
+    filter(str_detect(Property_ID, "ab"))%>%
+    count(Airbnb_HID)%>%
+    View()
+  
+  ## total number of listings scraped 2019 onwards
+  total <-  property_banff%>%
+    filter(str_detect(Property_ID, "ab"))%>%
+    filter(Scraped>=2019-01-01)%>%
+    count(Property_ID)
+  
+  total <- sum(total$n)
+  
+  ## housing breakdown - Listing_Type
+  property_banff%>%
+    st_drop_geometry()%>%
+    filter(str_detect(Property_ID, "ab"))%>%
+    filter(Scraped>=2019-01-01)%>%
+    count(Listing_Type)%>%
+    mutate(prop = n/total)
+  
+## TOFINO  
+  
+  
   ## filter out homeaway listings
   property_tofino%>%
     filter(str_detect(Property_ID, "ab"))
   
   ## Analysis: number of listings per host
   property_tofino%>%
+  filter(str_detect(Property_ID, "ab"))%>%
   count(Airbnb_HID)%>%
   View()
   
@@ -349,7 +433,6 @@ property_whistler <- property_whistler%>%
 
   total <- sum(total$n)
  
- 
   ## housing breakdown - Listing_Type
 property_tofino%>%
     st_drop_geometry()%>%
@@ -358,3 +441,120 @@ property_tofino%>%
     count(Listing_Type)%>%
     mutate(prop = n/total)
   
+## MONT TREMBLANT  
+
+## filter out homeaway listings
+property_mont_tremblant%>%
+  filter(str_detect(Property_ID, "ab"))
+
+## Analysis: number of listings per host
+property_tremblant%>%
+  filter(str_detect(Property_ID, "ab"))%>%
+  count(Airbnb_HID)%>%
+  View()
+
+## total number of listings scraped 2019 onwards
+total <-  property_mont_tremblant%>%
+  filter(str_detect(Property_ID, "ab"))%>%
+  filter(Scraped>=2019-01-01)%>%
+  count(Property_ID)
+
+total <- sum(total$n)
+
+## housing breakdown - Listing_Type
+property_mont_tremblant%>%
+  st_drop_geometry()%>%
+  filter(str_detect(Property_ID, "ab"))%>%
+  filter(Scraped>=2019-01-01)%>%
+  count(Listing_Type)%>%
+  mutate(prop = n/total)
+
+
+
+## BLUE MOUNTAIN  
+
+
+## filter out homeaway listings
+property_blue_mountains%>%
+  filter(str_detect(Property_ID, "ab"))
+
+## Analysis: number of listings per host
+property_blue_mountains%>%
+  filter(str_detect(Property_ID, "ab"))%>%
+  count(Airbnb_HID)%>%
+  View()
+
+## total number of listings scraped 2019 onwards
+total <-  property_blue_mountains%>%
+  filter(str_detect(Property_ID, "ab"))%>%
+  filter(Scraped>=2019-01-01)%>%
+  count(Property_ID)
+
+total <- sum(total$n)
+
+## housing breakdown - Listing_Type
+property_blue_mountains%>%
+  st_drop_geometry()%>%
+  filter(str_detect(Property_ID, "ab"))%>%
+  filter(Scraped>=2019-01-01)%>%
+  count(Listing_Type)%>%
+  mutate(prop = n/total)
+
+
+## WHISTLER
+
+## filter out homeaway listings
+property_whistler%>%
+  filter(str_detect(Property_ID, "ab"))
+
+## Analysis: number of listings per host
+property_whistler%>%
+  filter(str_detect(Property_ID, "ab"))%>%
+  count(Airbnb_HID)%>%
+  View()
+
+## total number of listings scraped 2019 onwards
+total <-  property_whistler%>%
+  filter(str_detect(Property_ID, "ab"))%>%
+  filter(Scraped>=2019-01-01)%>%
+  count(Property_ID)
+
+total <- sum(total$n)
+
+## housing breakdown - Listing_Type
+property_whistler%>%
+  st_drop_geometry()%>%
+  filter(str_detect(Property_ID, "ab"))%>%
+  filter(Scraped>=2019-01-01)%>%
+  count(Listing_Type)%>%
+  mutate(prop = n/total)
+
+
+## PEC  
+
+
+## filter out homeaway listings
+property_pec%>%
+  filter(str_detect(Property_ID, "ab"))
+
+## Analysis: number of listings per host
+property_pec%>%
+  filter(str_detect(Property_ID, "ab"))%>%
+  count(Airbnb_HID)%>%
+  View()
+
+## total number of listings scraped 2019 onwards
+total <-  property_pec%>%
+  filter(str_detect(Property_ID, "ab"))%>%
+  filter(Scraped>=2019-01-01)%>%
+  count(Property_ID)
+
+total <- sum(total$n)
+
+## housing breakdown - Listing_Type
+property_pec%>%
+  st_drop_geometry()%>%
+  filter(str_detect(Property_ID, "ab"))%>%
+  filter(Scraped>=2019-01-01)%>%
+  count(Listing_Type)%>%
+  mutate(prop = n/total)
