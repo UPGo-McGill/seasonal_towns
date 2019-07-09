336a72c4d4517b74d68927d74f1d5545e6e0e9c4
d-girard <danielle.girard@mail.mcgill.ca>
2019-07-09 14:13:38 -0400

    tofino

--- R/non_PEC.R
+++ R/non_PEC.R
@@ -231,6 +231,62 @@ property_pec <-
   select(-Property_Type)
 
 
+
+property_tofino <-
+  read_csv("data/tofino_property.csv", col_types = cols_only(
+    `Property_ID` = col_character(),
+    `Listing_Title` = col_character(),
+    `Property_Type` = col_character(),
+    `Listing_Type` = col_character(),
+    `Created_F` = col_skip(),
+    `Created` = col_date(format = ""),
+    `Scraped_F` = col_skip(),
+    `Scraped` = col_date(format = ""),
+    `Latitude` = col_double(),
+    `Longitude` = col_double(),
+    `Airbnb_PID` = col_double(),
+    `Airbnb_HID` = col_double()))%>%
+  set_names(c("Property_ID", "Listing_Title", "Property_Type", "Listing_Type",
+              "Created", "Scraped", "Latitude", "Longitude", "Airbnb_PID",
+              "Airbnb_HID")) %>% 
+  arrange(Property_ID) %>% 
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+  st_transform(32618) %>% 
+  filter(Property_Type %in% c(
+    "House", "Private room in house", "Apartment", "Cabin",
+    "Entire condominium", "Townhouse", "Condominium", "Entire apartment",
+    "Private room", "Loft", "Place", "Entire house", "Villa", "Guesthouse",
+    "Private room in apartment", "Guest suite", "Shared room in dorm",
+    "Chalet", "Dorm", "Entire chalet", "Shared room in loft", "Cottage",
+    "Resort", "Serviced apartment", "Other", "Bungalow", "Farm stay",
+    "Private room in villa", "Entire loft", "Entire villa",
+    "Private room in guesthouse", "Island", "Entire cabin", "Vacation home",
+    "Entire bungalow", "Earth house", "Nature lodge", "In-law",
+    "Entire guest suite", "Shared room in apartment", "Private room in loft",
+    "Tiny house", "Castle", "Earth House", "Private room in condominium",
+    "Entire place", "Shared room", "Hut", "Private room in guest suite",
+    "Private room in townhouse", "Timeshare", "Entire townhouse",
+    "Shared room in house", "Entire guesthouse", "Shared room in condominium",
+    "Cave", "Private room in cabin", "Dome house",
+    "Private room in vacation home", "Private room in dorm",
+    "Entire serviced apartment", "Private room in bungalow",
+    "Private room in serviced apartment", "Entire Floor", "Entire earth house",
+    "Entire castle", "Shared room in chalet", "Shared room in bungalow",
+    "Shared room in townhouse", "Entire cottage", "Private room in castle",
+    "Private room in chalet", "Private room in nature lodge", "Entire in-law",
+    "Shared room in guesthouse", "Casa particular", "Serviced flat", "Minsu",
+    "Entire timeshare", "Shared room in timeshare", "Entire vacation home",
+    "Entire nature lodge", "Entire island", "Private room in in-law",
+    "Shared room in serviced apartment", "Shared room in cabin", "Entire dorm",
+    "Entire cave", "Private room in timeshare", "Shared room in guest suite",
+    "Private room in cave", "Entire tiny house",
+    "Private room in casa particular (cuba)", "Casa particular (cuba)",
+    "Private room in cottage", "Private room in tiny house",
+    "Entire casa particular", "")) %>% 
+  select(-Property_Type)
+
+
+
 property_banff <- property_banff%>%
   mutate(region="Banff")
 property_blue_mountains <- property_blue_mountains%>%
@@ -254,41 +310,33 @@ property_whistler <- property_whistler%>%
   tm_shape(property_whistler)+
   tm_dots(col="black")
   
-prop_whistler <- property_whistler%>%
-  st_drop_geometry()
-prop_banff <- property_banff%>%
-  st_drop_geometry()
-prop_pec <- property_pec%>%
-  st_drop_geometry()
-prop_mont_t <- property_mont_tremblant%>%
-  st_drop_geometry()
-prop_blue_m <- property_blue_mountains%>%
-  st_drop_geometry()
+#prop_whistler <- property_whistler%>%
+#  st_drop_geometry()
+#prop_banff <- property_banff%>%
+#  st_drop_geometry()
+#prop_pec <- property_pec%>%
+#  st_drop_geometry()
+#prop_mont_t <- property_mont_tremblant%>%
+#  st_drop_geometry()
+#prop_blue_m <- property_blue_mountains%>%
+#  st_drop_geometry()
+#prop_tofino <- property_tofino%>%
+#  st_drop_geometry()
 
 
-props <- rbind(property_banff, property_blue_mountains, property_mont_tremblant, property_pec, property_whistler)
+#props <- rbind(property_banff, property_blue_mountains, property_mont_tremblant, property_pec, property_whistler)
 
-props%>%
-  group_by(region)
+#props%>%
+#  group_by(region)
 
-regions <- factor(c("Banff","Mont Tremblant","Blue Mountains","Whistler", "PEC"))
-props <- data.frame(regions= regions)
-
-
-active2019 <- numeric(5)
-active2019[1] <- 
-props <- props%>%
-  data.frame()
+#regions <- factor(c("Banff","Mont Tremblant","Blue Mountains","Whistler", "PEC"))
+#props <- data.frame(regions= regions)
 
 
-tst <- property_mont_tremblant%>%
+  ## Analysis: number of listings per host
+  
+  property_tofino%>%
   count(Airbnb_HID)%>%
   View()
   
-tst%>%
-  filter(n>=10)
-View()
-max(tst$n)
 
-tst%>%
-  filter(n==36)
