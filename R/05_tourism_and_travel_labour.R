54bc70ef43c16488ffa1fe88bc288f61c493a4c3
d-girard <danielle.girard@mail.mcgill.ca>
2019-07-25 12:15:40 -0400

    select variables and weighting

--- R/05_tourism_and_travel_labour.R
+++ R/05_tourism_and_travel_labour.R
@@ -3,47 +3,47 @@
 ca_561520 <-
   read_csv("data/561520.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 561520)
   
 ca_561510 <-
   read_csv("data/561510.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 561510)
 
 
 ca_561590 <-
   read_csv("data/561590.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 561590)
 
 
 ca_721198 <-
   read_csv("data/721198.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 721198)
 
 
 ca_721192 <-
   read_csv("data/721192.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 721192)
 
 
 ca_721191 <-
   read_csv("data/721191.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 721191)
 
 
@@ -51,139 +51,147 @@ ca_721191 <-
 ca_721120<-
   read_csv("data/721120.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 721120)
 
 
 ca_721114 <-
   read_csv("data/721114.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 721114)
 
 
 ca_721113 <-
   read_csv("data/721113.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 721113)
 
 
 ca_721111 <-
   read_csv("data/721111.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 721111)
 
 
 ca_713990 <-
   read_csv("data/713990.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 713990)
 
 
 ca_487110 <-
   read_csv("data/487110.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 487110)
 
 
 ca_487210 <-
   read_csv("data/487210.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 487210)
 
 
 ca_487990 <-
   read_csv("data/487990.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 487990)
 
 
 ca_713920 <-
   read_csv("data/713920.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 713920)
 
 
 ca_713930 <-
   read_csv("data/713930.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 713930)
 
 
 ca_713940 <-
   read_csv("data/713940.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 713940)
 
 
 ca_722210 <-
   read_csv("data/722210.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 722210)
 
 ca_722110 <-
   read_csv("data/722110.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 722110)
 
 ca_721310 <-
   read_csv("data/721310.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 721310)
 
 ca_721213 <-
   read_csv("data/721213.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 721213)
 
 ca_721212 <-
   read_csv("data/721212.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 721212)
 
 
 ca_721211 <-
   read_csv("data/721211.csv")%>%
   set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
-              "1-4", "10-19", "100-199", "20-49", "200-499",
-              "5-9", "50-99", "500+"))%>%
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
   mutate(code= 721211)
 
+ca_712120 <-
+  read_csv("data/712120.csv")%>%
+  set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
+  mutate(code= 712120)
 
-
-
-
-
+ca_712190 <-
+  read_csv("data/712190.csv")%>%
+  set_names(c("PROV_CSD", "Total", "Subtotal", "Indeterminate",
+              "1_4", "10_19", "100_199", "20_49", "200_499",
+              "5_9", "50_99", "500plus"))%>%
+  mutate(code= 712190)
 
 df <- rbind(ca_561510,ca_561520, ca_561590,
             ca_713990, 
@@ -192,11 +200,15 @@ df <- rbind(ca_561510,ca_561520, ca_561590,
             ca_721120, ca_721191, ca_721192, ca_721198, ca_487110, ca_487210, 
             ca_487990, ca_713920, ca_713930, ca_713940, ca_721211, 
             ca_721212, 
-            ca_721213, ca_721310, ca_722110, ca_722210)
+            ca_721213, ca_721310, ca_722110, ca_722210,
+            ca_712120, ca_712190)
 
 df <- df%>%
   filter(str_detect(PROV_CSD, "-"))%>%
   separate(PROV_CSD, c("GeoUID", "name"), " - ")
+
+codes <- df
+
 df$name <- NULL
 df$code <- NULL
 
@@ -204,18 +216,37 @@ result <- df %>%
   group_by(GeoUID) %>% 
   summarise_each(list(sum))
 
-tst <- inner_join(result,canada)
-tst <- tst%>%
+result <- inner_join(result,canada)
+result <- result%>%
   mutate(tourism_prop= Total/ Population)
 
-tst%>%
+result%>%
   filter(Population>=1000)%>%
   View()
 
-library(stringr)
+#### ADD WEIGHTING FOR BUSINESS SIZE
 
-df%>%
-  filter(str_detect(name, "Golden"), Total>3)
+weighted <- df%>%
+  mutate(weighting = `100_199`*0.2 +`200_499`*0.33+`500plus`*0.4)
+
+weighted <- weighted %>% 
+  group_by(GeoUID) %>% 
+  mutate(total2 =Total+weighting )%>%
+  summarise_each(list(sum))
+
+weighted <- inner_join(weighted,canada)
+weighted <- weighted%>%
+  mutate(tourism_prop= total2/ Population)
+
+weighted%>%
+  filter(Population>=1000)%>%
+  View()
+
+
+###
+
+codes%>%
+  filter(str_detect(name, "Thompson-Nicola A"), Total>1)
 
 inner_join(result,canada)
 
